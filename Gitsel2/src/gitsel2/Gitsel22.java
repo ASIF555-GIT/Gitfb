39c53fd3b7ee83525109e76ca0eb0adda54ea58c
ASIF <asifadoor18@gmail.com>
2024-06-26 11:19:52 +0530

    facebook

---
+++ Gitsel2/src/gitsel2/Gitsel22.java
@@ -0,0 +1,14 @@
+package gitsel2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Gitsel22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\asifa\\Downloads\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();	
+		driver.get("https:\\www.facebook.com");
+	}
+}
